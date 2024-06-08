7b1b290996a2504b756c7d6c0bec86e805244039
Anandito Satria Asyraf <ananditosatriaa181@gmail.com>
2024-06-08 16:39:36 +0700

    [BUG] Catalog Adjustment *ShopName

--- lib/page/catalogpage.dart
+++ lib/page/catalogpage.dart
@@ -4,16 +4,23 @@ import '../API/api_service.dart';
 
 class CatalogPage extends StatelessWidget {
   final int shopId;
+  final String namaToko;
 
-  CatalogPage({required this.shopId});
+  const CatalogPage({
+    super.key, // Using super parameter for key
+    required this.shopId,
+    required this.namaToko,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Catalog Toko $shopId',
-        style: TextStyle(color: Colors.white),),
-         backgroundColor: const Color.fromRGBO(134, 28, 30, 1),
+        title: Text(
+          'Catalog Toko $namaToko',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromRGBO(134, 28, 30, 1),
       ),
       body: FutureBuilder<List<Menu>>(
         future: ApiService.fetchMenuByShop(shopId),
@@ -21,32 +28,31 @@ class CatalogPage extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
+            return Center(child: Text('${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return Center(child: Text('No products found'));
           } else {
             return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(height: 10),
-                GridView.builder(
-                  padding: EdgeInsets.all(16.0),
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.75,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  GridView.builder(
+                    padding: EdgeInsets.all(16.0),
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.75,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      Menu Menus = snapshot.data![index];
+                      return _buildProductCard(context, Menus);
+                    },
                   ),
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    Menu Menus = snapshot.data![index];
-                    return _buildProductCard(context, Menus);
-                  },
-                ),
-              ]
-            );
+                ]);
           }
         },
       ),
@@ -54,111 +60,117 @@ class CatalogPage extends StatelessWidget {
   }
 
   Widget _buildProductCard(BuildContext context, Menu Menu) {
-  return GestureDetector(
-    onTap: () {
-      // Add your onTap logic here
-    },
-    child: Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: Colors.grey[300]!),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.25),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: Offset(0, 3), // changes position of shadow
-          ),
-        ],
-      ),
-      child: Stack(
-        children: <Widget>[
-          // Background image
-          Container(
-            height: 400.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              image: DecorationImage(
-                image: NetworkImage(
-                  Menu.imageMenu != null
-                      ? 'http://10.0.2.2:8001/api/${Menu.imageMenu}'
-                      : 'https://fivestar.sirv.com/example.jpg?profile=Example',
-                ),
-                fit: BoxFit.cover,
-              ),
+    return GestureDetector(
+      onTap: () {
+        // Add your onTap logic here
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: Colors.grey[300]!),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3), // changes position of shadow
             ),
-          ),
-          // Text and other content
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
+          ],
+        ),
+        child: Stack(
+          children: <Widget>[
+            // Background image
+            Container(
+              height: 400.0,
               decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.8),
-                borderRadius: BorderRadius.vertical(bottom: Radius.circular(8)),
-              ),
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 236, 19, 4).withOpacity(0.8),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        child: Text('\Rp.${Menu.hargaMenu}',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.add),
-                        onPressed: () async {
-                          try {
-                            int? bookingId = await ApiService.getBookingIdByUser();
-                            if (bookingId == null) {
-                              // Handle the case where the booking ID is not found
-                              throw Exception('Booking ID not found');
-                            }
-                            _showModal(context, Menu, bookingId);
-                          } catch (e) {
-                            // Handle any errors that occur
-                            print('Error: $e');
-                          }
-                        },
-                      ),
-                    ],
+                borderRadius: BorderRadius.circular(8),
+                image: DecorationImage(
+                  image: NetworkImage(
+                    Menu.imageMenu != null
+                        ? 'http://10.0.2.2:8001/api/${Menu.imageMenu}'
+                        : 'https://fivestar.sirv.com/example.jpg?profile=Example',
                   ),
-                  SizedBox(height: 8),
-                  Text(
-                    Menu.namaMenu,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "Stok: "+Menu.stokMenu.toString(),
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    Menu.deskripsiMenu,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: TextStyle(fontSize: 12),
-                  ),
-                  SizedBox(height: 8),
-                ],
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-        ],
+            // Text and other content
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(8)),
+                ),
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 236, 19, 4)
+                                .withOpacity(0.8),
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: Text(
+                            '\Rp.${Menu.hargaMenu}',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.add),
+                          onPressed: () async {
+                            try {
+                              int? bookingId =
+                                  await ApiService.getBookingIdByUser();
+                              if (bookingId == null) {
+                                // Handle the case where the booking ID is not found
+                                throw Exception('Booking ID not found');
+                              }
+                              _showModal(context, Menu, bookingId);
+                            } catch (e) {
+                              // Handle any errors that occur
+                              print('Error: $e');
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      Menu.namaMenu,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Stok: " + Menu.stokMenu.toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      Menu.deskripsiMenu,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    SizedBox(height: 8),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
+
   void _showModal(BuildContext context, Menu product, int bookingId) {
     int quantity = 1;
 
@@ -189,10 +201,12 @@ class CatalogPage extends StatelessWidget {
                 onPressed: () async {
                   try {
                     await ApiService.addCart(bookingId, product.id, quantity);
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Added to cart')));
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text('Added to cart')));
                     Navigator.pop(context); // Close modal
                   } catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to add to cart: $e')));
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Failed to add to cart: $e')));
                   }
                 },
                 child: Text('Order'), // Order button
